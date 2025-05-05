a73f6956de83c9b581cc3e4c511155851a493566
Carlos3du <carlos.mec2005@gmail.com>
2025-05-05 11:16:08 -0300

    add: funcion export

---
+++ functions/distance_calc.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from scipy.spatial.distance import cdist
+import pickle
+
+
+with open('./pickles/scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+
+
+
+def calculate_distances(new_user_data, data_grouped):
+    """
+    Calcula as distâncias entre um novo usuário e as plantas existentes e retorna o ranking das 4 plantas mais próximas.
+
+    Args:
+        new_user_data (dict): Dados do novo usuário no formato de dicionário.
+        data_distances (pd.DataFrame): Dados normalizados das plantas existentes.
+        data_grouped (pd.DataFrame): Dados agrupados das plantas com informações adicionais.
+
+    Returns:
+        pd.DataFrame: Ranking das 4 plantas mais próximas com informações de compatibilidade.
+    """
+   
+    new_user = pd.DataFrame([new_user_data])
+    new_user_normalized = scaler.transform(new_user)
+
+    data_distances = data_grouped.drop(columns=['kmeans_group', 'name', 'group_name'])
+
+    distances = cdist(new_user_normalized, data_distances, metric='euclidean')
+
+
+    distances_df = pd.DataFrame(distances.T, columns=['Distance'])
+    distances_df['Plant Index'] = data_distances.index
+    distances_df['Plant Name'] = data_grouped.loc[data_distances.index, 'name'].values
+    distances_df['Group'] = data_grouped.loc[data_distances.index, 'group_name'].values
+
+
+    distances_df['Compatibility Percentage'] = round(
+        100 - (distances_df['Distance'] / distances_df['Distance'].max() * 100), 0
+    )
+
+
+    return distances_df.sort_values(by='Distance').head(4)
+
+
+
+
+"""
+# Exemplo de uso da função
+
+new_user_data = {
+    'ind_pets': 0,
+    'ind_apartment': 0,
+    'size_code': 1,
+    'experience_level_code': 1,
+    'disponibility_level_code': 1
+}
+
+
+data_grouped = pd.read_csv('./datasets/results/plants_grouped.csv') 
+
+
+ranking = calculate_distances(new_user_data, data_grouped)
+print(ranking)
+"""
